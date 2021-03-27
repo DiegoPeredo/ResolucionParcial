2edce4b14f1232aaf03ebadbd0e18c7783fa054a
Diego Peredo <a2019118858@estudiantes.upsa.edu.bo>
2021-03-27 11:40:48 -0400

    Se aumentó Post al AddController

--- RecuperacionDiego/RecuperacionDiego/Controllers/AddController.cs
+++ RecuperacionDiego/RecuperacionDiego/Controllers/AddController.cs
@@ -10,7 +10,15 @@ namespace RecuperacionDiego.Controllers
     public class AddController : ApiController
     {
         [HttpGet]
+        [HttpPost]
         public int Sumar(int a, int b)
+        {
+            return a + b;
+        }
+
+        [HttpPost]
+        [Route("{a}/{b}")]
+        public int Sumar2(int a, int b)
         {
             return a + b;
         }
